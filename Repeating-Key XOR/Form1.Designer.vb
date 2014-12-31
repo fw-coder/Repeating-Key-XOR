d6df9cd4b7b3cec971f6e4873938a265b64e1619
Brian Harris <aircav11@gmail.com>
2014-12-31 10:56:52 -0600

    Fix output; Replace input textBox with RichTextBox to detect line feeds and returns in the text

--- Repeating-Key XOR/Form1.Designer.vb
+++ Repeating-Key XOR/Form1.Designer.vb
@@ -25,10 +25,10 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
         Me.SuspendLayout()
         '
         'Button1
@@ -56,15 +56,6 @@ Partial Class Form1
         Me.Label1.Size = New System.Drawing.Size(79, 13)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Repeating key:"
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox2.Location = New System.Drawing.Point(12, 61)
-        Me.TextBox2.Multiline = True
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(627, 123)
-        Me.TextBox2.TabIndex = 3
         '
         'Label2
         '
@@ -94,15 +85,23 @@ Partial Class Form1
         Me.TextBox3.Size = New System.Drawing.Size(627, 123)
         Me.TextBox3.TabIndex = 5
         '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 61)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(627, 133)
+        Me.RichTextBox1.TabIndex = 7
+        Me.RichTextBox1.Text = ""
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(651, 384)
+        Me.Controls.Add(Me.RichTextBox1)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.TextBox3)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
@@ -115,9 +114,9 @@ Partial Class Form1
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
 
 End Class
--- Repeating-Key XOR/Form1.vb
+++ Repeating-Key XOR/Form1.vb
@@ -3,13 +3,17 @@
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
         ' Getting repeating key and text to encrypt
         Dim key As Byte() = System.Text.ASCIIEncoding.ASCII.GetBytes(TextBox1.Text)
-        Dim txt As Byte() = System.Text.ASCIIEncoding.ASCII.GetBytes(TextBox2.Text)
+        Dim txt As Byte() = System.Text.ASCIIEncoding.ASCII.GetBytes(RichTextBox1.Text)
         Dim counter As Integer = 0
 
         For i As Integer = 0 To txt.Length - 1 Step 3
             For j As Integer = 0 To key.Length - 1
                 Dim out As Byte = txt(counter) Xor key(j)
-                TextBox3.Text = TextBox3.Text & out ' this is wrong - needs to be converted to hex string
+                If counter = 0 And out < &H10 Then
+                    TextBox3.Text = TextBox3.Text & "0" & Convert.ToString(out, 16) 'add leading 0 to first byte
+                Else
+                    TextBox3.Text = TextBox3.Text & Convert.ToString(out, 16)
+                End If
                 counter = counter + 1
                 If counter = txt.Length Then Exit For
             Next
