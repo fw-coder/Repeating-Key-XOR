5d1d3f552da63246ce81ea401b9453b9e85268fb
Brian Harris <aircav11@gmail.com>
2015-01-04 02:04:22 -0600

    add file output

--- Repeating-Key XOR/Form1.vb
+++ Repeating-Key XOR/Form1.vb
@@ -1,14 +1,22 @@
-﻿Public Class Form1
+﻿Imports System
+Imports System.IO
+Imports System.Text
+
+Public Class Form1
+
 
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
         ' Getting repeating key and text to encrypt
         Dim key As Byte() = System.Text.ASCIIEncoding.ASCII.GetBytes(TextBox1.Text)
         Dim txt As Byte() = System.Text.ASCIIEncoding.ASCII.GetBytes(RichTextBox1.Text)
+        Dim fileOut(txt.Length) As Byte
+
         Dim counter As Integer = 0
 
         For i As Integer = 0 To txt.Length - 1 Step key.Length
             For j As Integer = 0 To key.Length - 1
                 Dim out As Byte = txt(counter) Xor key(j)
+                fileOut(counter) = out
                 If out < &H10 Then
                     RichTextBox3.Text = RichTextBox3.Text & "0" & Convert.ToString(out, 16) 'add leading 0 to first byte
                 Else
@@ -19,5 +27,9 @@
             Next
         Next
 
+        Dim path As String = "c:\ttemp\MyTest.txt"
+        Dim fs As FileStream = File.Create(path)
+        fs.Write(fileOut, 0, fileOut.Length)
+        fs.Close()
     End Sub
 End Class
